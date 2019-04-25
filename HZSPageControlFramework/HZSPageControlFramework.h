95f45f359021cc54215d3d20471971fbf796ea3e
黄子汕 <zishan.huang@office.feng.com>
2019-04-25 16:43:34 +0800

    add framework

---
+++ HZSPageControlFramework/HZSPageControlFramework.h
@@ -0,0 +1,19 @@
+//
+//  HZSPageControlFramework.h
+//  HZSPageControlFramework
+//
+//  Created by Acan on 2019/4/25.
+//  Copyright © 2019 hzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HZSPageControlFramework.
+FOUNDATION_EXPORT double HZSPageControlFrameworkVersionNumber;
+
+//! Project version string for HZSPageControlFramework.
+FOUNDATION_EXPORT const unsigned char HZSPageControlFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HZSPageControlFramework/PublicHeader.h>
+
+#import "HZSPageControl.h"
